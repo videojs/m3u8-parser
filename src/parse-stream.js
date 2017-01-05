87ff894a5321f08b1c6246a4964e49259bb40f80
Jordi Cenzano <jcenzano@brightcove.com>
2017-01-06 00:06:44 +0100

    Necessary change to process #ZEN-TOTAL-DURATION tag

--- src/parse-stream.js
+++ src/parse-stream.js
@@ -102,7 +102,7 @@ export default class ParseStream extends Stream {
     }
 
     // Comments
-    if (line.indexOf('#EXT') !== 0) {
+    if ((line.indexOf('#EXT') !== 0) && (line.indexOf('#ZEN') !== 0)){
       this.trigger('data', {
         type: 'comment',
         text: line.slice(1)
