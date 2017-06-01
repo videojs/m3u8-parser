f52e05dcb7e3737cb5779cfa9b03bfe07257b81c
Gary Katsevman <git@gkatsev.com>
2017-06-01 11:49:17 -0400

    update to rollup generator 5

---
+++ scripts/umd.rollup.config.js
@@ -0,0 +1,43 @@
+/**
+ * Rollup configuration for packaging the plugin in a module that is consumable
+ * as the `src` of a `script` tag or via AMD or similar client-side loading.
+ *
+ * This module DOES include its dependencies.
+ */
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  moduleName: 'm3u8-parser',
+  entry: 'src/index.js',
+  dest: 'dist/m3u8-parser.js',
+  format: 'umd',
+  legacy: true,
+  plugins: [
+    resolve({
+      browser: true,
+      main: true,
+      jsnext: true
+    }),
+    json(),
+    commonjs({
+      sourceMap: false
+    }),
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [
+        ['es2015', {
+          loose: true,
+          modules: false
+        }]
+      ],
+      plugins: [
+        'external-helpers',
+        'transform-object-assign'
+      ]
+    })
+  ]
+};
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,48 +1,34 @@
 module.exports = function(config) {
-  var browsers = config.browsers;
-  var frameworks = ['qunit'];
-  var plugins = ['karma-qunit'];
-
-  var addBrowserLauncher = function(browser) {
-    plugins.push('karma-' + browser.toLowerCase() + '-launcher');
+  var detectBrowsers = {
+    enabled: false,
+    usePhantomJS: false
   };
 
-  // On Travis CI, we can only run in Firefox.
+  // On Travis CI, we can only run in Firefox and Chrome; so, enforce that.
   if (process.env.TRAVIS) {
-    browsers = ['Firefox'];
-    browsers.forEach(addBrowserLauncher);
+    config.browsers = ['Firefox', 'travisChrome'];
+  }
 
-  // If specific browsers are requested on the command line, load their
-  // launchers.
-  } else if (browsers.length) {
-    browsers.forEach(addBrowserLauncher);
-
-  // If no browsers are specified, we will do a `karma-detect-browsers` run,
-  // which means we need to set up that plugin and all the browser plugins
-  // we are supporting.
-  } else {
-    frameworks.push('detectBrowsers');
-    plugins.push('karma-detect-browsers');
-    ['chrome', 'firefox', 'ie', 'safari'].forEach(addBrowserLauncher);
+  // If no browsers are specified, we enable `karma-detect-browsers`
+  // this will detect all browsers that are available for testing
+  if (!config.browsers.length) {
+    detectBrowsers.enabled = true;
   }
 
   config.set({
     basePath: '..',
-    frameworks: frameworks,
-
+    frameworks: ['qunit', 'detectBrowsers'],
     files: [
       'node_modules/sinon/pkg/sinon.js',
-      'node_modules/sinon/pkg/sinon-ie.js',
       'test/dist/bundle.js'
     ],
-
-    browsers: browsers,
-    plugins: plugins,
-
-    detectBrowsers: {
-      usePhantomJS: false
+    customLaunchers: {
+      travisChrome: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     },
-
+    detectBrowsers: detectBrowsers,
     reporters: ['dots'],
     port: 9876,
     colors: true,
