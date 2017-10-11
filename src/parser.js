cbb37628b02760fe15747eedfae740d8f2114979
Matthew Neil <mjneil@users.noreply.github.com>
2017-10-11 11:06:24 -0400

    update comment

--- src/parser.js
+++ src/parser.js
@@ -269,8 +269,9 @@ export default class Parser extends Stream {
             'program-date-time'() {
               if (typeof this.manifest.dateTimeString === 'undefined') {
                 // PROGRAM-DATE-TIME is a media-segment tag, but for backwards
-                // compatibility, we add the first occurance of the PROGRAM-DATE-TIME tag
+                // compatibility, we add the first occurence of the PROGRAM-DATE-TIME tag
                 // to the manifest object
+                // TODO: Consider removing this in future major version
                 this.manifest.dateTimeString = entry.dateTimeString;
                 this.manifest.dateTimeObject = entry.dateTimeObject;
               }
