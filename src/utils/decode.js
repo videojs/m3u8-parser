beb80296397203086b7337d3bfac7cac14b4c5c5
Matheus Macabu <macabu.matheus@gmail.com>
2019-08-14 13:06:50 +0200

    chore: add NodeJS support for encrypted playlists
    Currently if you try to parse a playlist with EXT-X-KEY
    tags it will fail, since `decodeB64ToUint8Array` uses
    a `window.atob` function which does not exist in NodeJS.
    
    Added a check if the function is undefined, and use the
    `Buffer.from` implementation.

--- src/utils/decode.js
+++ src/utils/decode.js
@@ -1,7 +1,9 @@
 import window from 'global/window';
 
 export default function decodeB64ToUint8Array(b64Text) {
-  const decodedString = window.atob(b64Text || '');
+  const decodedString = window.atob === undefined ?
+    Buffer.from(b64Text || '', 'base64').toString('binary') :
+    window.atob(b64Text || '');
   const array = new Uint8Array(decodedString.length);
 
   for (let i = 0; i < decodedString.length; i++) {
