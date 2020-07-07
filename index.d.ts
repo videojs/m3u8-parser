a021326a42fbdd7cc1d48c94baca086730333ac0
Henning Hall <henning.hall@hotmail.com>
2020-07-07 17:09:39 +0200

    Add typescript types

---
+++ index.d.ts
@@ -0,0 +1,70 @@
+export class Parser {
+    push: (string: string) => void;
+    end: () => void;
+    manifest: {
+      allowCache: boolean,
+      endList: boolean,
+      mediaSequence: number,
+      discontinuitySequence: number,
+      playlistType: string,
+      custom: {},
+      playlists: [
+        {
+          attributes: {},
+          Manifest
+        }
+      ],
+      mediaGroups: {
+        AUDIO: {
+          'GROUP-ID': {
+            NAME: {
+              default: boolean,
+              autoselect: boolean,
+              language: string,
+              uri: string,
+              instreamId: string,
+              characteristics: string,
+              forced: boolean
+            }
+          }
+        },
+        VIDEO: {},
+        'CLOSED-CAPTIONS': {},
+        SUBTITLES: {}
+      },
+      dateTimeString: string,
+      dateTimeObject: Date,
+      targetDuration: number,
+      totalDuration: number,
+      discontinuityStarts: [number],
+      segments: [
+        {
+          byterange: {
+            length: number,
+            offset: number
+          },
+          duration: number,
+          attributes: {},
+          discontinuity: number,
+          uri: string,
+          timeline: number,
+          key: {
+            method: string,
+            uri: string,
+            iv: string
+          },
+          map: {
+            uri: string,
+            byterange: {
+              length: number,
+              offset: number
+            }
+          },
+          'cue-out': string,
+          'cue-out-cont': string,
+          'cue-in': string,
+          custom: {}
+        }
+      ]
+  }
+}
