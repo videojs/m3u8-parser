a58149de5480524ea2587de29d6d212bfb3119a6
Brandon Casey <2381475+brandonocasey@users.noreply.github.com>
2021-01-12 15:30:55 -0500

    fix: cjs dist should import only cjs (#120)

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,4 +1,5 @@
 const generate = require('videojs-generate-rollup-config');
+const replace = require('@rollup/plugin-replace');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
@@ -6,6 +7,23 @@ const options = {
   input: 'src/index.js',
   externals(defaults) {
     defaults.module.push('@videojs/vhs-utils');
+
+    return defaults;
+  },
+  primedPlugins(defaults) {
+    // when using "require" rather than import
+    // require cjs module
+    defaults.replace = replace({
+      // single quote replace
+      "require('@videojs/vhs-utils/es": "require('@videojs/vhs-utils/cjs",
+      // double quote replace
+      'require("@videojs/vhs-utils/es': 'require("@videojs/vhs-utils/cjs'
+    });
+
+    return defaults;
+  },
+  plugins(defaults) {
+    defaults.module.unshift('replace');
 
     return defaults;
   }
