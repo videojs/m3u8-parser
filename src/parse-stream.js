843dbfa34361dbb934ae5942ec29e788026bac80
Logan Song <logan.song@endeavorstreaming.com>
2023-11-15 15:52:04 +0800

    EXT-X-CUE-IN ":" is not necessary

--- src/parse-stream.js
+++ src/parse-stream.js
@@ -429,7 +429,7 @@ export default class ParseStream extends Stream {
         this.trigger('data', event);
         return;
       }
-      match = (/^#EXT-X-CUE-IN:(.*)?$/).exec(newLine);
+      match = (/^#EXT-X-CUE-IN:?(.*)?$/).exec(newLine);
       if (match) {
         event = {
           type: 'tag',
